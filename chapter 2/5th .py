7df3f585f0d59a757d81cce56285e761d2fba45a
Debojyotiaidev <debojyotirock43@gmail.com>
2026-08-21 19:21:20 +0530

    chapter2 is done

---
+++ chapter 2/5th .py
@@ -0,0 +1,5 @@
+#Write a python program to find an average of two numbers entered by the user.
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=int(a)+int(b)/2
+print("Average of two numbers is: ",c)
---
+++ chapter 2/6th.py
@@ -0,0 +1,5 @@
+#Write a python program to find an average of two 
+# numbers entered by the user.
+a=input("Enter first number: ")
+b=int(a)*int(a)
+print("the square root is ",b)

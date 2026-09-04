8a3b6280081cadbced281de7dcb6260c06486694
Debojyotiaidev <debojyotirock43@gmail.com>
2026-09-04 13:31:40 +0530

    bro i did some shit with cuz i was practicing some tts

--- 1stcode.py
+++ 1stcode.py
@@ -1,1 +1,11 @@
-print("hello world")
+import edge_tts
+import asyncio
+
+async def speak():
+    text = "I love you"
+    voice = "en-US-JennyNeural"
+
+    communicate = edge_tts.Communicate(text, voice)
+    await communicate.save("love.mp3")
+
+asyncio.run(speak())
